d863183e599fb0fc6c48039ce659292c5ed0f1e1
JuliansRecent <juliansrecent@gmail.com>
2023-02-26 11:51:59 -0800

    Module complete. Create a terminal based program that runs a loop asking for a number until the number 4 is inputted, then break.

--- src/main/java/AreWeThereYet.java
+++ src/main/java/AreWeThereYet.java
@@ -6,5 +6,20 @@ public class AreWeThereYet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Create a while loop set to true
+        while(true) {
+            // Prompt the user for an input
+            System.out.println("Give a number:");
+
+            // Store the variable in the int val
+            int val = Integer.valueOf(scanner.nextLine());
+
+            // Break the loop if the value is equal to 4
+            if(val == 4) {
+                break;
+            }
+        }
+
+
     }
 }
